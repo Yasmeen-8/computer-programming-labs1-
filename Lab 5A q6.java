9319cf0ec859565e61722caf65b87ebf1f15876a
Yasmeen hani <yasmeen.hani@outlook.com>
2025-04-21 08:31:59 +0300

    Update Lab 5A q6.java

--- Lab 5A q6.java
+++ Lab 5A q6.java
@@ -1,1 +1,9 @@
 
+public static void simpleWhileLoop(int num){ // name of the method 
+    int i = 1; // intiate the i
+    
+    while (i <= num) {
+        System.out.println(i); // print the number
+        i++; // add one
+    } //final
+}
