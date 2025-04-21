c5a275e10af0f35ec269aaf1b71fc0feec38dce6
Yasmeen hani <yasmeen.hani@outlook.com>
2025-04-21 08:27:11 +0300

    Create Lab 5A q2.java

---
+++ Lab 5A q2.java
@@ -0,0 +1,8 @@
+public static void simpleDoWhile(){
+    int i = 1; // intiate i 
+    do {
+        System.out.println(i); // print the i 
+        i++;
+    }
+    while (i <= 10); // the while statement 
+}//final 
